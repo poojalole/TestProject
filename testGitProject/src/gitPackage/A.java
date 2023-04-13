be6a6b5c84092837c9fa5a0b43da806b4c7c3adb
DELL <DELL@WINCTRL-44U6VLM>
2023-04-13 17:06:17 +0530

    Added print statement

--- testGitProject/src/gitPackage/A.java
+++ testGitProject/src/gitPackage/A.java
@@ -5,7 +5,9 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello Automation");
-		
+		System.out.println("Hello Automation");
+		System.out.println("Hello Automation");
+		System.out.println("Hello Automation");
 	}
 
 }
